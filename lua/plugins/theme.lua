be8bf8d4f6efd4449e06a71f281df29ae6c4333b
Loc Le <lpl212757@gmail.com>
2026-04-19 23:18:52 +0700

    feat(theme): switch active colorscheme from catppuccin to vague
    Add vague.nvim as the active theme with transparent background enabled.
    Catppuccin kept as lazy-loaded fallback (no longer sets colorscheme on startup).

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,23 +1,37 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    priority = 1000, -- load before everything else
-    config = function()
-        require("catppuccin").setup({
-            flavour = "mocha",
-            transparent_background = false,
-            integrations = {
-                nvimtree = true,
-                telescope = { enabled = true },
-                gitsigns = true,
-                indent_blankline = { enabled = true },
-                cmp = true,
-                treesitter = true,
-                bufferline = true,
-                mini = { enabled = true },
-            },
-        })
-        vim.cmd.colorscheme("catppuccin")
-    end,
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = true,
+        priority = 1000, -- load before everything else
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha",
+                transparent_background = false,
+                integrations = {
+                    nvimtree = true,
+                    telescope = { enabled = true },
+                    gitsigns = true,
+                    indent_blankline = { enabled = true },
+                    cmp = true,
+                    treesitter = true,
+                    bufferline = true,
+                    mini = { enabled = true },
+                },
+            })
+        end,
+    },
+    {
+        "vague-theme/vague.nvim",
+        lazy = false,
+        priority = 1000, -- load before everything else
+        config = function()
+            require('vague').setup({
+              transparent = true, -- If true, background is not set
+              bold = true, -- Disable bold globally
+              italic = false, -- Disable italic globally
+            })
+            vim.cmd.colorscheme('vague')
+        end,
+    }
 }
