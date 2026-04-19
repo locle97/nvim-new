e1e68d907eb6b3d8b26369e383f7d9a3f925b06f
Loc Le <lpl212757@gmail.com>
2026-04-19 23:19:04 +0700

    Update lualine to vague

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -17,7 +17,7 @@ return {
 
         require("lualine").setup({
             options = {
-                theme = "catppuccin-mocha",
+                theme = "vague",
                 globalstatus = true,
                 component_separators = { left = "", right = "" },
                 section_separators = { left = "", right = "" },
