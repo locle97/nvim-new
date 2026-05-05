33d856f9e0a41b912444bde43a90938ff97821e4
Loc Le <lpl212757@gmail.com>
2026-05-06 00:54:20 +0700

    refactor(lspconfig): remove ts_ls Vue typescript-plugin override
    Drop the hardcoded @vue/typescript-plugin path and Vue filetype extension
    for ts_ls, relying on default ts_ls configuration instead.

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -76,19 +76,6 @@ end
 -- Additional servers + per-server overrides
 local servers = { "html", "cssls", "omnisharp", "jsonls", "ts_ls" }
 
-vim.lsp.config("ts_ls", {
-    init_options = {
-        plugins = {
-            {
-                name = "@vue/typescript-plugin",
-                location = "/usr/local/lib/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin",
-                languages = { "javascript", "typescript", "vue" },
-            },
-        },
-    },
-    filetypes = { "javascript", "typescript", "vue" },
-})
-
 vim.lsp.enable(servers)
 
 return M
